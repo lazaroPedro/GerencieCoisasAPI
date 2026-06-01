65dcf3ceecb60c9e85173822cf8e5ac9a5b9925c
Caio-Al <caionascimento60@gmail.com>
2026-06-01 16:59:46 -0300

    feat(api): registra admin de movimentacoes

--- api/movimentacoes/admin.py
+++ api/movimentacoes/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Movimentacao
 
+
+@admin.register(Movimentacao)
+class MovimentacaoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'produto', 'tipo', 'quantidade', 'data', 'observacao']
+    list_filter = ['tipo', 'data']
+    search_fields = ['produto__nome', 'observacao']
+    readonly_fields = ['data']
+    ordering = ['-data']
 # Register your models here.
