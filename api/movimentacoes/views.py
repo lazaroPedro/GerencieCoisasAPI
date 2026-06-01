7d446c733fd86490907839b4f85238325fc16e99
Caio-Al <caionascimento60@gmail.com>
2026-06-01 17:46:33 -0300

    correções e bugs

--- api/movimentacoes/views.py
+++ api/movimentacoes/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import MovimentacaoSerializer
 from .models import Movimentacao
 from rest_framework.response import Response
