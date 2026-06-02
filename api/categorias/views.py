78873e64b004ff7c138c6b193ad17863936944c5
Lázaro Pedro <lazaro.p806@gmail.com>
2026-06-01 21:37:29 -0300

    add allow any

--- api/categorias/views.py
+++ api/categorias/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated,  AllowAny
 from .serializers import CategoriaSerializer
 from .models import Categoria
 
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
--- api/produtos/views.py
+++ api/produtos/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 
 from rest_framework.viewsets import ModelViewSet
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, AllowAny
 from .serializers import ProdutoSerializer
 from .models import Produto
 
 class ProdutoViewSet(ModelViewSet):
     queryset = Produto.objects.all()
     serializer_class = ProdutoSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [AllowAny]
